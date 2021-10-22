3a703d0765af4f9362db0e87dd18535a449a3c6d
Jordan P <jordan.pruede@gmail.com>
2021-10-22 21:00:28 +0200

    Moved ServiceLifetime in its own file.

--- DIContainer/Descriptors/ServiceDescriptor.cs
+++ DIContainer/Descriptors/ServiceDescriptor.cs
@@ -142,23 +142,4 @@ namespace DIContainer.Descriptors
             where T : class
             => new(typeof(T), factory, ServiceLifetime.Transient);
     }
-
-    /// <summary>
-    /// Different 
-    /// </summary>
-    public enum ServiceLifetime
-    {
-        /// <summary>
-        /// Creates a new Service only once during the application lifetime, and uses it everywhere.
-        /// </summary>
-        Singleton,
-        /// <summary>
-        /// Creates a new instance for every scope. (Each "request" is a Scope). Within the scope, it reuses the existing service.
-        /// </summary>
-        Scoped,
-        /// <summary>
-        /// Creates a new instance of the service every time you request it.
-        /// </summary>
-        Transient
-    }
 }
